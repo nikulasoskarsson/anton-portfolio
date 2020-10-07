fb43887985089d4e26872e4f414ea32f25ba1cbb
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-07 09:22:28 +0200

    rm warning for build

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ import Navbar from './components/sections/header/Navbar';
 import Portfolio from './components/sections/portfolio';
 
 function App() {
-  const updateScrollPosition = () => console.log('scrolled');
-  const [activeSection, setActiveSection] = useState(1);
+  // const updateScrollPosition = () => console.log('scrolled');
+  // const [activeSection, setActiveSection] = useState(1);
 
   // document.addEventListener('scroll', () => {
-  //   // console.log(document.body.getBoundingClientRect());
   //   if (document.body.getBoundingClientRect().top < -300) {
   //     setActiveSection(2);
   //   }
   // });
 
   return (
-    <div className='App' onScrollCapture={updateScrollPosition}>
+    <div className='App'>
       <Navbar />
-      <ScrollPosition activeSection={activeSection} />
+      <ScrollPosition activeSection={1} />
       <Header />
       <Portfolio />
 
