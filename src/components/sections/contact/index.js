712f44200757f6adaf2203c8843306f3856b5399
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-08 17:46:39 +0200

    testing out form in netlify

--- src/components/sections/contact/index.js
+++ src/components/sections/contact/index.js
@@ -1,7 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import IconRight from './IconRight';
 
 const Contact = () => {
-  return <div className='contact'>Contact section</div>;
+  const [formData, setFormData] = useState([
+    {
+      label: 'What is your name?',
+      for: 'name',
+      placement: 1,
+    },
+    {
+      label: 'What is your email?',
+      for: 'email',
+      placement: 2,
+    },
+    {
+      label: 'What is your subject?',
+      for: 'subject',
+      placement: 3,
+    },
+    {
+      label: 'What is your message?',
+      for: 'message',
+      placement: 4,
+    },
+  ]);
+
+  const [formPlacement, setFormPlacement] = useState(0);
+
+  return (
+    <div className='contact' id='contact'>
+      <h1 className='heading-h1'>
+        Got a project, or do you just want to say hi?
+      </h1>
+      <form
+        className='contact__form'
+        id='contact-form'
+        name='contact-us'
+        method='POST'
+        data-netlify='true'
+        action='/contact-success.html'
+      >
+        <input type='hidden' value='contact-us' name='form-name' />
+        <label className='contact__label' htmlFor={'input'}>
+          {formData[formPlacement].label}
+          {formPlacement < 3 && (
+            <div onClick={() => setFormPlacement(formPlacement + 1)}>
+              <IconRight />
+            </div>
+          )}
+        </label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          className={
+            formData[formPlacement].for === 'name'
+              ? 'contact__border'
+              : 'display-hidden'
+          }
+        />
+        <input
+          type='text'
+          id='email'
+          name='email'
+          className={
+            formData[formPlacement].for === 'email'
+              ? 'contact__border'
+              : 'display-hidden'
+          }
+        />
+        <input
+          type='text'
+          id='subject'
+          name='subject'
+          className={
+            formData[formPlacement].for === 'subject'
+              ? 'contact__border'
+              : 'display-hidden'
+          }
+        />
+
+        <input
+          type='text'
+          id='message'
+          name='message'
+          className={
+            formData[formPlacement].for === 'message'
+              ? 'contact__border'
+              : 'display-hidden'
+          }
+        />
+
+        <p className='contact__form-placement'>
+          {formData[formPlacement].placement} / 4
+        </p>
+        {formPlacement === 3 && <button class='button'>Send</button>}
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/sections/header/Header.js
+++ src/components/sections/header/Header.js
@@ -5,15 +5,7 @@ const Header = () => {
   return (
     <div className='header' id='header'>
       <h1 className='heading-h1'>Hello my name is Anton & I'M a designer</h1>
-      <video
-        width='300px'
-        height='300px'
-        loop
-        autoPlay
-        style={{ background: 'white' }}
-      >
-        <source src={require('./video.mp4')} type='video/mp4' />
-      </video>
+      <img style={{ width: '300px' }} src={require('./video.gif')} alt='' />
       <DropdownIcon />
     </div>
   );
