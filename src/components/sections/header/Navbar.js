ad20782f2c7b92adc82eb36953566791aa58455f
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-31 14:04:53 +0100

    looking better on mobile

--- src/components/sections/header/Navbar.js
+++ src/components/sections/header/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Scrollspy from 'react-scrollspy'
 import { HashLink as Link } from 'react-router-hash-link'
 
